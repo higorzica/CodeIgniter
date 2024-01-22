41adbdbae5b3480f4ad32132e4dff31a52e090e0
Higor Zica <higorbhe@gmail.com>
2024-01-22 21:53:25 +0100

    tree commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,9 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/ver_projeto/(:num)', 'Home::ver_projeto/$1');
+
+$routes->get('/login', 'Login::index');
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Views/ver_projeto.php
@@ -0,0 +1,86 @@
+<?= $this->extend('layouts/main_layout') ?>
+
+
+<?= $this->section('content') ?>
+
+<?php if(empty($verprojetos)): ?> 
+<section class="text-gray-600 body-font">
+  <div class="bg-white flex justify-center align-center text-center">
+    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Não existem mais fotos deste projeto!</h1>
+  </div>
+</section>
+
+<?php else: ?>
+<section class="text-gray-600 body-font">
+
+<div class="bg-white">
+  <div class="pt-6">
+    <nav aria-label="Breadcrumb">
+      <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <li>
+          <div class="flex items-center">
+            <a href="<?= site_url("/") ?>" class="mr-2 text-sm font-medium text-gray-900">Home</a>
+            <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-5 w-4 text-gray-300">
+              <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+            </svg>
+          </div>
+        </li>
+        <li>
+          <div class="flex items-center">
+            <a href="#" class="mr-2 text-sm font-medium text-gray-900">Projeto: <?= $verprojetos[0]['titulo'] ?></a>
+            <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" aria-hidden="true" class="h-5 w-4 text-gray-300">
+              <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+            </svg>
+          </div>
+        </li>
+      </ol>
+    </nav>
+
+    <!-- Image gallery -->
+    <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
+        <?php foreach ($verprojetos as $projeto): ?>
+            <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
+                <img src="<?= base_url("projetos/" . $projeto['nomearquivo']) ?>" alt="Two each of gray, white, and black shirts laying flat." class="h-full w-full object-cover object-center">
+            </div>
+      <?php endforeach; ?>
+
+    </div>
+
+    <!-- Product info -->
+    <div class="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+      <div class="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+        <h1 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl"> <?= $verprojetos[0]['titulo'] ?> </h1>
+      </div>
+
+      <!-- Options -->
+
+
+      <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
+        <!-- Description and details -->
+        <div>
+          <div class="space-y-6">
+            <p class="text-base text-gray-900"><?= $verprojetos[0]['descricao'] ?></p>
+          </div>
+        </div>
+
+        <div class="mt-10">
+          <h3 class="text-sm font-medium text-gray-900">Highlights</h3>
+
+          <div class="mt-4">
+            <ul role="list" class="list-disc space-y-2 pl-4 text-sm">
+              <li class="text-gray-400"><span class="text-gray-600">Hand cut and sewn locally</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Dyed with our proprietary colors</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Pre-washed &amp; pre-shrunk</span></li>
+              <li class="text-gray-400"><span class="text-gray-600">Ultra-soft 100% cotton</span></li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<?php endif; ?>
+
+<?= $this->endSection() ?>
