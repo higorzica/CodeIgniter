4d7d6b73f1064103494b7e5181fce2d898aa244b
Higor Zica <higorbhe@gmail.com>
2024-01-24 16:03:32 +0100

    atz css and home

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,8 +10,8 @@
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Olá - Seja Bem-vindo 🤝 ! Eu Sou Higor Zica </h1>
-      <p class="mb-8 leading-relaxed">Meu nome é Higor Zica, sou técnico em informática e bacharel em Admnistração. Sou fascinado por programação e principalmente pelo desenvolvimento web/mobile, estando sempre a procura de conhecimento e novos desafios.</p>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-zinc-50">Olá - Seja Bem-vindo 🤝 ! Eu Sou Higor Zica </h1>
+      <p class="mb-8 text-zinc-300 leading-relaxed">Meu nome é Higor Zica, sou técnico em informática e bacharel em Admnistração. Sou fascinado por programação e principalmente pelo desenvolvimento web/mobile, estando sempre a procura de conhecimento e novos desafios.</p>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Entrar em contato</button>
       </div>
@@ -22,17 +22,17 @@
 <section class="text-gray-600 body-font" id="projetos">
   <div class="container px-5 py-12 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">PROJETOS</h1>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-zinc-50">PROJETOS</h1>
     </div>
-    <div class="flex flex-wrap -m-4">
+    <div class="grid grid-cols-1 lg:grid-cols-2 flex-wrap gap-3 -m-4">
     <?php foreach ($projetos as $projeto): ?>
-        <div class="p-4 lg:w-1/2">
+        <div class="p-4 rounded-md" style="background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 30%, rgba(63,40,181,1) 62%, rgba(0,66,111,1) 100%);">
             <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-            <img alt="team" class="flex-shrink-0 rounded-lg w-80 h-80 object-contain object-center sm:mb-0 mb-4" src="<?= base_url("projetos/" . $projeto->foto) ?>">
+              <img alt="team" class="flex-shrink-0 rounded-lg w-80 h-80 object-contain object-center sm:mb-0 mb-4" src="<?= base_url("projetos/" . $projeto->foto) ?>">
                 <div class="flex-grow sm:pl-8">
-                        <h2 class="title-font font-medium text-lg text-gray-900"><?= $projeto->titulo ?></h2>
+                        <h2 class="title-font font-medium text-lg text-zinc-50"><?= $projeto->titulo ?></h2>
                         <h3 class="text-gray-500 mb-3">UI Developer</h3>
-                        <p class="mb-4"><?= $projeto->descricao ?></p>
+                        <p class="mb-4 text-zinc-300"><?= $projeto->descricao ?></p>
                         <span class="inline-flex">
                             <a class="text-gray-500">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
